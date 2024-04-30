077d9c9a12065301d591a95023b581a5317e239f
Gbnobre <166552655+Gbnobre@users.noreply.github.com>
2024-04-30 14:36:46 -0300

    Add files via upload

---
+++ Dicionario ingles-portugues.py
@@ -0,0 +1,4 @@
+dicionario = {'peace':'paz', 'love':'amor', 'shirt':'camisa', 'place':'lugar', 'you':'você', 'mind':'mente','sky':'céu','go':'vai','people':'pessoas','eye':'olho','environment':'ambiente','equally':'igualmente','mouse':'rato','plane':'avião','work':'trabalho','mixed':'mistura','keys':'chaves','moon':'lua','right':'certo','twenty':'vinte'}
+
+for ingles, portugues in dicionario. items():
+    print(f'Inglês > {ingles} Português > {portugues}')
