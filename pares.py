73f7e335f7ddcee31c634f7edbf4676d9fd65311
Gbnobre <166552655+Gbnobre@users.noreply.github.com>
2024-06-04 17:17:37 -0300

    Add files via upload

---
+++ pares.py
@@ -0,0 +1,17 @@
+#fim = int(input('Digite um número:'))
+#n = 0
+
+#while n <= fim:
+ #   if n % 2 == 0:
+  #      print('n: %d' % (n))
+   # n = n+1
+
+n = 0
+soma = 0
+
+while n < 10:
+    calculadora = int(input('Digite um número: '))
+    n = n+1
+    soma = calculadora + soma
+
+print('o resultado da soma dos 10 numeros é', soma)
